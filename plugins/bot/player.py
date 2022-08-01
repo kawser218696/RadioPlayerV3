58f8a0c2bc2f00976e260f936d0672bc7aeb4208
MD KAWSER MHAMUD <76180115+kawser218696@users.noreply.github.com>
2022-08-01 15:56:06 +0600

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -573,5 +573,5 @@ async def not_chat(_, m: Message):
                 InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
             ]
          ]
-    k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
+    k=await m.reply_photo(photo="https://te.legra.ph/file/fcb6342511b544b1905dd.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/Kawser218696) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
